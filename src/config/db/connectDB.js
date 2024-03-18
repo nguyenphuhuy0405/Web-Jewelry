16614c887740ab31c4026893c00b5c1d56a36c5f
nguyenphuhuy0405 <nguyenphuhuybgg@gmail.com>
2024-03-18 22:13:30 +0700

    "Connect to Cluster0 in MongoDB Alats"

--- src/config/db/connectDB.js
+++ src/config/db/connectDB.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
         console.log('Connected to MongoDB')
     } catch (error) {
         console.log('Connected failed')
